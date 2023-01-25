ac9abf634c467d09453ee426bb69f23252d4c34d
Benjamin Perseghetti <bperseghetti@rudislabs.com>
2023-01-24 22:29:54 -0500

    More cleanup to remove notebooks.
    Signed-off-by: Benjamin Perseghetti <bperseghetti@rudislabs.com>

--- notebooks/build_venv.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-
-python3 -m venv --prompt cerebri $SCRIPT_DIR/../.venv-cerebri
-source $SCRIPT_DIR/../.venv-cerebri/bin/activate
-python3 -m pip install -r requirements.txt

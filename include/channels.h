00209dfbb98fdbedd0b63bb3b268c340db200417
James Goppert <james.goppert@gmail.com>
2023-01-26 14:57:10 -0500

    Add odometry sensor.
    Signed-off-by: Benjamin Perseghetti <bperseghetti@rudislabs.com>
    
    Co-authored-by: James Goppert <james.goppert@gmail.com>

--- include/channels.h
+++ include/channels.h
@@ -4,13 +4,22 @@
 #include <zephyr/zbus/zbus.h>
 #include "messages.h"
 
-ZBUS_CHAN_DECLARE(chan_actuators);
+// sensor measurements
 ZBUS_CHAN_DECLARE(chan_accelerometer);
 ZBUS_CHAN_DECLARE(chan_gyroscope);
 ZBUS_CHAN_DECLARE(chan_magnetometer);
 ZBUS_CHAN_DECLARE(chan_altimeter);
 ZBUS_CHAN_DECLARE(chan_navsat);
-ZBUS_CHAN_DECLARE(chan_waypoint);
+ZBUS_CHAN_DECLARE(chan_external_odometry);
+
+// commands
 ZBUS_CHAN_DECLARE(chan_rc_input);
+ZBUS_CHAN_DECLARE(chan_waypoint);
+
+// estimation
+ZBUS_CHAN_DECLARE(chan_estimator_odometry);
+
+// control
+ZBUS_CHAN_DECLARE(chan_actuators);
 
 #endif /* DC9A7AA6_4867_45B4_B139_1F16E724ECC6 */

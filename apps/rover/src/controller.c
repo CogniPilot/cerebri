393141b08ad6dd926a8e9ccab037bf7e016c7d18
Benjamin Perseghetti <bperseghetti@rudislabs.com>
2023-02-01 11:28:56 -0500

    Switch between manual and trajectory mode.
    Signed-off-by: Benjamin Perseghetti <bperseghetti@rudislabs.com>

--- apps/rover/src/controller.c
+++ apps/rover/src/controller.c
@@ -16,6 +16,7 @@ struct msg_odometry_t msg_odometry;
 
 double dx = 0;
 double dy = 0;
+int last_mode_change;
  
 void listener_controller_callback(const struct zbus_channel *chan) {
     
@@ -23,12 +24,23 @@ void listener_controller_callback(const struct zbus_channel *chan) {
         const struct msg_rc_input_t *msg_rc_input = (const struct msg_rc_input_t *)zbus_chan_const_msg(chan);
         struct msg_rc_input_t msg_control = *msg_rc_input;
 
-        msg_control.thrust = dx + msg_rc_input->thrust;
-        msg_control.yaw = dy + msg_rc_input->yaw;
-	printf("dx: %15.2f\n", dx);
-	printf("dy: %15.2f\n", dy);
+        if (msg_rc_input->mode == 1) {
+            if (msg_rc_input->mode != last_mode_change) {
+                last_mode_change = msg_rc_input->mode;
+                printf("Set to manual mode!\n");
+            }
+            msg_control.thrust = msg_rc_input->thrust;
+            msg_control.yaw = msg_rc_input->yaw;
+        } else if (msg_rc_input->mode == 0) {
+            if (msg_rc_input->mode != last_mode_change) {
+                last_mode_change = msg_rc_input->mode;
+                printf("Set to trajectory follow!\n");
+            }
+            msg_control.thrust = dx;
+            msg_control.yaw = dy;
+        }
 
-	struct msg_actuators_t actuators_msg = mixer(&msg_control);
+	    struct msg_actuators_t actuators_msg = mixer(&msg_control);
         zbus_chan_pub(&chan_actuators, &actuators_msg, K_NO_WAIT);
     } else if (chan == &chan_gyroscope) {
         const struct msg_gyroscope_t * msg_gyroscope = (const struct msg_gyroscope_t *)zbus_chan_const_msg(chan);

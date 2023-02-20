7c756884e42bdb8d6455119c9d82364f39d10592
James Goppert <james.goppert@gmail.com>
2023-02-19 19:49:11 -0500

    Clean up module use.
    Signed-off-by: James Goppert <james.goppert@gmail.com>

--- launch/cerebri.launch.py
+++ launch/cerebri.launch.py
@@ -6,7 +6,7 @@ from pathlib import Path
 
 def generate_launch_description():
     cerebri_path = Path(get_package_prefix('cerebri'))
-    cerebri_bin = cerebri_path / 'lib' / 'cerebri' / 'zephyr.elf'
+    cerebri_bin = cerebri_path / 'lib' / 'cerebri' / 'cerebri'
     cerebri_cmd = f"terminator -u -T cerebri --geometry=500x700+0+0 -e {cerebri_bin} 2>&1"
     return LaunchDescription([
         ExecuteProcess(

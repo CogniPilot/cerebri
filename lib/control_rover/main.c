9d8aa5854b7a789e7e2171ab59c09993e964200d
James Goppert <james.goppert@gmail.com>
2023-05-22 00:53:34 -0400

    Add cmd_vel control.
    Signed-off-by: James Goppert <james.goppert@gmail.com>

--- lib/control_rover/main.c
+++ lib/control_rover/main.c
@@ -13,24 +13,18 @@
 #define MY_STACK_SIZE 10240
 #define MY_PRIORITY 4
 
-static double thrust = 0;
-static double yaw = 0;
-static Actuators msg = Actuators_init_zero;
+static Odometry g_odometry = Odometry_init_zero;
+static Joy g_joy = Joy_init_zero;
+static Twist g_cmd_vel = Twist_init_zero;
 
 static void listener_control_rover_callback(const struct zbus_channel* chan)
 {
     if (chan == &chan_in_joy) {
-        printf("received joy\n");
-        Joy* msg = (Joy*)(chan->message);
-        thrust = msg->axes[1];
-        yaw = msg->axes[3];
+        g_joy = *(Joy*)(chan->message);
     } else if (chan == &chan_in_odometry) {
-        printf("received odometry\n");
-        Odometry * msg = (Odometry*)(chan->message);
-        printf("x: %10.4f, y: %10.4f, z: %10.4f\n",
-                msg->pose.pose.position.x,
-                msg->pose.pose.position.y,
-                msg->pose.pose.position.z);
+        g_odometry = *(Odometry*)(chan->message);
+    } else if (chan == &chan_in_cmd_vel) {
+        g_cmd_vel = *(Twist*)(chan->message);
     }
 }
 
@@ -38,25 +32,42 @@ ZBUS_LISTENER_DEFINE(listener_control_rover, listener_control_rover_callback);
 
 static void control_entry_point(void* p1, void* p2, void* p3)
 {
+    int mode = 0;
+    double scale = 30.0;
+    static Actuators msg = Actuators_init_zero;
+    msg.velocity_count = 4;
+    msg.has_header = true;
+    strncpy(msg.header.frame_id, "test", 16);
+    msg.header.has_stamp = true;
+    msg.header.seq = 0;
+    msg.header.stamp.nanos = 0;
+    msg.header.stamp.seconds = 0;
+    msg.normalized_count = 0;
+    msg.position_count = 0;
+
     while (true) {
-        // send data to motors
-        msg.has_header = true;
-        strncpy(msg.header.frame_id, "test", 16);
-        msg.header.has_stamp = true;
-        msg.header.seq = 0;
-        msg.header.stamp.nanos = 0;
-        msg.header.stamp.seconds = 0;
-        msg.normalized_count = 0;
-        msg.position_count = 0;
-        double scale = 30.0;
-        msg.velocity[0] = scale * (1.0 * thrust + 1.0 * yaw);
-        msg.velocity[1] = scale * (1.0 * thrust - 1.0 * yaw);
-        msg.velocity[2] = scale * (1.0 * thrust - 1.0 * yaw);
-        msg.velocity[3] = scale * (1.0 * thrust + 1.0 * yaw);
-        msg.velocity_count = 4;
+        if (mode == 0) {
+            // joystick mode
+            double thrust = g_joy.axes[1];
+            double yaw = g_joy.axes[3];
+            msg.velocity[0] = scale * (1.0 * thrust + 1.0 * yaw);
+
+            msg.velocity[1] = scale * (1.0 * thrust - 1.0 * yaw);
+            msg.velocity[2] = scale * (1.0 * thrust - 1.0 * yaw);
+            msg.velocity[3] = scale * (1.0 * thrust + 1.0 * yaw);
+        } else if (mode == 1) {
+            // cmd_vel mode
+            double thrust = g_cmd_vel.linear.x;
+            double yaw = g_cmd_vel.angular.z;
+            msg.velocity[0] = scale * (1.0 * thrust + 1.0 * yaw);
+            msg.velocity[1] = scale * (1.0 * thrust - 1.0 * yaw);
+            msg.velocity[2] = scale * (1.0 * thrust - 1.0 * yaw);
+            msg.velocity[3] = scale * (1.0 * thrust + 1.0 * yaw);
+        }
+
+        // sleep to set control rate at 10 Hz
         zbus_chan_pub(&chan_out_actuators, &msg, K_SECONDS(1));
         k_msleep(1e3 / 10);
-        // sleep to set control rate at 10 Hz
     }
 }
 
--- lib/synapse_zbus/channels.c
+++ lib/synapse_zbus/channels.c
@@ -20,7 +20,11 @@ ZBUS_CHAN_DEFINE(chan_in_cmd_vel, // Name
     Twist, // Message type
     NULL, // Validator
     NULL, // User Data
-    ZBUS_OBSERVERS(), // observers
+    ZBUS_OBSERVERS(
+#if defined(CONFIG_CONTROL_ROVER)
+        listener_control_rover,
+#endif
+        ), // observers
     ZBUS_MSG_INIT(0) // Initial value {0}
 );
 

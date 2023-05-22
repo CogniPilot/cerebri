96e36d9d2a3e987e275559f42a0037e7a033c1c5
James Goppert <james.goppert@gmail.com>
2023-05-22 18:53:03 -0400

    Add rover control back.
    Signed-off-by: James Goppert <james.goppert@gmail.com>

--- lib/control_rover/main.c
+++ lib/control_rover/main.c
@@ -10,64 +10,228 @@
 
 #include <synapse_zbus/channels.h>
 
+#include "casadi/rover.h"
+#include "parameters.h"
+
 #define MY_STACK_SIZE 10240
 #define MY_PRIORITY 4
 
-static Odometry g_odometry = Odometry_init_zero;
-static Joy g_joy = Joy_init_zero;
+
+enum control_mode_t {
+    MODE_MANUAL=0,
+    MODE_AUTO=1,
+    MODE_CMD_VEL=2,
+};
+typedef enum control_mode_t control_mode_t;
+char * mode_name[3] = {"manual", "auto", "cmd_vel"};
+
+control_mode_t g_mode = {MODE_MANUAL};
+bool g_armed = false;
+static Odometry g_pose  = Odometry_init_zero;
 static Twist g_cmd_vel = Twist_init_zero;
+static BezierTrajectory g_bezier_trajectory = BezierTrajectory_init_zero;
+
+static void handle_joy(Joy * joy) {
+    // arming
+    if (joy->buttons[7] == 1) {
+        g_armed = true;
+    } else if (joy->buttons[6] == 1) {
+        g_armed = false;
+    }
+
+    // handle modes
+    control_mode_t prev_mode = g_mode;
+    if (joy->buttons[0] == 1) {
+        g_mode =  MODE_MANUAL;
+    } else if (joy->buttons[1] == 1) {
+        if (g_bezier_trajectory.time_start != 0) {
+            g_mode =  MODE_AUTO;
+        } else {
+            printf("no valid tranjectory\n");
+        }
+    } else if (joy->buttons[2] == 1) {
+        g_mode =  MODE_CMD_VEL;
+    }
+
+    // notify on mode change
+    if (g_mode != prev_mode) {
+        printf("mode changed to %s!\n", mode_name[g_mode]);
+    }
+
+    // translate joystick to twist message
+    if (g_mode == MODE_MANUAL) {
+        g_cmd_vel.linear.x = joy->axes[1];
+        g_cmd_vel.angular.z = joy->axes[3];
+    }
+}
 
 static void listener_control_rover_callback(const struct zbus_channel* chan)
 {
     if (chan == &chan_in_joy) {
-        g_joy = *(Joy*)(chan->message);
+        handle_joy((Joy*)(chan->mutex));
     } else if (chan == &chan_in_odometry) {
-        g_odometry = *(Odometry*)(chan->message);
+        g_pose = *(Odometry*)(chan->message);
     } else if (chan == &chan_in_cmd_vel) {
         g_cmd_vel = *(Twist*)(chan->message);
+    } else if (chan == &chan_in_bezier_trajectory) {
+        g_bezier_trajectory = *(BezierTrajectory*)(chan->message);
     }
 }
 
 ZBUS_LISTENER_DEFINE(listener_control_rover, listener_control_rover_callback);
 
-static void control_entry_point(void* p1, void* p2, void* p3)
-{
-    int mode = 0;
-    double scale = 30.0;
-    static Actuators msg = Actuators_init_zero;
-    msg.velocity_count = 4;
-    msg.has_header = true;
-    strncpy(msg.header.frame_id, "test", 16);
-    msg.header.has_stamp = true;
-    msg.header.seq = 0;
-    msg.header.stamp.nanos = 0;
-    msg.header.stamp.seconds = 0;
-    msg.normalized_count = 0;
-    msg.position_count = 0;
+// computes rc_input from V, omega
+void mixer() {
+
+    // given cmd_vel, compute actuators
+    double V = g_cmd_vel.linear.x;
+    double omega = g_cmd_vel.angular.z;
+    Actuators actuators = Actuators_init_zero;
+
+    // casadi mem args
+    casadi_int * iw = NULL;
+    casadi_real * w = NULL;
+    int mem = 0;
+
+#ifdef CONFIG_CONTROL_ROVER_STEERING_ACKERMANN
+    /* ackermann_steering:(L,omega,V)->(delta) */
+    {
+
+        double delta = 0;
+        const casadi_real * args[3];
+        casadi_real * res[1];
+        args[0] = &wheel_base;
+        args[1] = &omega;
+        args[2] = &V;
+        res[0] = &delta;
+        ackermann_steering(args, res, iw, w, mem);
+        double omega_fwd = V/wheel_radius;
+        double turn_angle = 0;
+        if (fabs(V) > 0.01) {
+            turn_angle = delta;
+        }
+        actuators.position[0] = turn_angle;
+        actuators.velocity[0] = omega_fwd;
+    }
+#endif
+
+#ifdef CONFIG_CONTROL_ROVER_STEERING_DIFFERENTIAL
+    /* differential_steering:(L,omega,w)->(Vw) */
+    {
+        double Vw = 0;
+        const casadi_real * args[3];
+        casadi_real * res[1];
+        args[0] = &wheel_base;
+        args[1] = &omega;
+        args[2] = &wheel_separation;
+        res[0] = &Vw;
+        differential_steering(args, res, iw, w, mem);
+        double omega_fwd = V/wheel_radius;
+        double omega_turn = Vw/wheel_radius;
+        actuators.velocity[0] = omega_fwd - omega_turn;
+        actuators.velocity[1] = omega_fwd + omega_turn;
+        actuators.velocity[2] = omega_fwd + omega_turn;
+        actuators.velocity[3] = omega_fwd - omega_turn;
+    }
+#endif
+
+    zbus_chan_pub(&chan_out_actuators, &actuators, K_NO_WAIT);
+}
+
+// computes thrust/steering in auto mode
+void auto_mode() {
+    // goal -> given position goal, find cmd_vel
+    
+    uint64_t time_start = g_bezier_trajectory.time_start;
+    uint64_t time_stop = g_bezier_trajectory.time_stop;
+    int64_t uptime = k_uptime_get()*1e6;
+    int64_t T_nsec = time_stop - time_start;
+    int64_t t_nsec = uptime - time_start;
+    if (t_nsec > T_nsec) {
+      // stop
+      g_cmd_vel.linear.x = 0;
+      g_cmd_vel.angular.z = 0;
+      return;
+    }
+    if (t_nsec < 0) {
+      t_nsec = 0;
+    }
+    double t = t_nsec*1e-9;
+    double T = T_nsec*1e-9;
+    double x, y, psi, V, omega = 0;
+    double e[3] = {}; // e_x, e_y, e_theta
+  
+    double PX[6], PY[6];
+    for (int i=0; i<6; i++) {
+        PX[i] = g_bezier_trajectory.x[i];
+        PY[i] = g_bezier_trajectory.y[i];
+    }
+
+    // casadi mem args
+    casadi_int * iw = NULL;
+    casadi_real * w = NULL;
+    int mem = 0;
+
+    /* bezier6_rover:(t,T,PX[1x6],PY[1x6],L)->(x,y,psi,V,omega) */
+    {
+        const casadi_real * args[5];
+        casadi_real * res[5];
+        args[0] = &t;
+        args[1] = &T;
+        args[2] = PX;
+        args[3] = PY;
+        args[4] = &wheel_base;
+        res[0] = &x;
+        res[1] = &y;
+        res[2] = &psi;
+        res[3] = &V;
+        res[4] = &omega;
+        bezier6_rover(args, res, iw, w, mem);
+    }
+
+    /* se2_error:(p[3],r[3])->(error[3]) */
+    {
+        const casadi_real * args[2];
+        casadi_real * res[1];
+
+        double p[3], r[3];
+
+        // vehicle position
+        p[0] = g_pose.pose.pose.position.x;
+        p[1] = g_pose.pose.pose.position.y;
+        p[2] = 2*atan2(
+                g_pose.pose.pose.orientation.z,
+                g_pose.pose.pose.orientation.w);
+
+        // reference position
+        r[0] = x;
+        r[1] = y;
+        r[2] = psi;
+
+        // call function
+        args[0] = p;
+        args[1] = r;
+        res[0] = e;
+        se2_error(args, res, iw, w, mem);
+    }
+
+    // compute twist
+    g_cmd_vel.linear.x = V + wheel_radius*gain_along_track*e[0];
+    g_cmd_vel.angular.z = omega + gain_cross_track*e[1] + gain_heading*e[2];
+}
+
+void control_entry_point(void * p1, void * p2, void * p3) {
 
     while (true) {
-        if (mode == 0) {
-            // joystick mode
-            double thrust = g_joy.axes[1];
-            double yaw = g_joy.axes[3];
-            msg.velocity[0] = scale * (1.0 * thrust + 1.0 * yaw);
-
-            msg.velocity[1] = scale * (1.0 * thrust - 1.0 * yaw);
-            msg.velocity[2] = scale * (1.0 * thrust - 1.0 * yaw);
-            msg.velocity[3] = scale * (1.0 * thrust + 1.0 * yaw);
-        } else if (mode == 1) {
-            // cmd_vel mode
-            double thrust = g_cmd_vel.linear.x;
-            double yaw = g_cmd_vel.angular.z;
-            msg.velocity[0] = scale * (1.0 * thrust + 1.0 * yaw);
-            msg.velocity[1] = scale * (1.0 * thrust - 1.0 * yaw);
-            msg.velocity[2] = scale * (1.0 * thrust - 1.0 * yaw);
-            msg.velocity[3] = scale * (1.0 * thrust + 1.0 * yaw);
+        // auto
+        if (g_mode == MODE_AUTO) {
+            auto_mode();
         }
 
-        // sleep to set control rate at 10 Hz
-        zbus_chan_pub(&chan_out_actuators, &msg, K_SECONDS(1));
-        k_msleep(1e3 / 10);
+        mixer();
+
+        // sleep to set control rate at 50 Hz
+        k_usleep(1e6/50);
     }
 }
 
---
+++ lib/control_rover/parameters.h
@@ -0,0 +1,17 @@
+#ifndef E00B391E_08D8_47AC_B779_57B422AF3850
+#define E00B391E_08D8_47AC_B779_57B422AF3850
+
+//gains
+const double gain_heading = 0.8;
+const double gain_cross_track = 1.0;
+const double gain_along_track = 1.0;
+
+// parameters
+const double wheel_radius = 0.108;
+const double wheel_base = 0.667;
+
+const double wheel_separation = 0.667;
+
+#endif /* E00B391E_08D8_47AC_B779_57B422AF3850 */
+
+

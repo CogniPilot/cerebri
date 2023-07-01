081fe55ae7d37a4dde92da785f42fbd161423e1d
James Goppert <james.goppert@gmail.com>
2023-07-01 17:10:06 +0000

    Add imu zbus channel
    Closes #63
    
    Signed-off-by: James Goppert <james.goppert@gmail.com>
    Signed-off-by: Benjamin Perseghetti <bperseghetti@rudislabs.com>

--- drivers/dream/sitl/zephyr_main.c
+++ drivers/dream/sitl/zephyr_main.c
@@ -8,8 +8,10 @@
 #include <synapse_tinyframe/TinyFrame.h>
 
 #include <pb_encode.h>
-#include <synapse_protobuf/clock.pb.h>
+#include <synapse_protobuf/sim_clock.pb.h>
 #include <synapse_tinyframe/utils.h>
+
+#include <zephyr/sys/ring_buffer.h>
 
 #define MY_STACK_SIZE 500
 #define MY_PRIORITY -10
@@ -23,18 +25,20 @@ static struct private_module_context g_priv = {
     .module_name = "dream_sitl_zephyr",
 };
 
-extern synapse_msgs_Clock g_sim_clock;
+extern synapse_msgs_SimClock g_sim_clock;
 extern TinyFrame g_tf;
 extern bool g_clock_initialized;
-extern synapse_msgs_Timestamp g_clock_offset;
+extern synapse_msgs_Time g_clock_offset;
 extern synapse_msgs_NavSatFix g_in_nav_sat_fix;
+extern synapse_msgs_Imu g_in_imu;
+extern struct ring_buf g_msg_updates;
 
 void listener_dream_sitl_callback(const struct zbus_channel* chan)
 {
     if (chan == &chan_out_actuators) {
         TF_Msg msg;
         TF_ClearMsg(&msg);
-        uint8_t buf[500];
+        uint8_t buf[synapse_msgs_Actuators_size];
         pb_ostream_t stream = pb_ostream_from_buffer((pu8)buf, sizeof(buf));
         int status = pb_encode(&stream, synapse_msgs_Actuators_fields, chan->message);
         if (status) {
@@ -54,7 +58,7 @@ static void zephyr_sim_entry_point(void)
     printf("%s: zephyr sim entry point\n", g_priv.module_name);
     printf("%s: waiting for sim clock\n", g_priv.module_name);
     while (true) {
-        synapse_msgs_Clock sim_clock;
+        synapse_msgs_SimClock sim_clock;
         struct timespec request, remaining;
         request.tv_sec = 1;
         request.tv_nsec = 0;
@@ -74,19 +78,31 @@ static void zephyr_sim_entry_point(void)
 
     printf("%s: running main loop\n", g_priv.module_name);
     while (true) {
-        zbus_chan_pub(&chan_in_nav_sat_fix, &g_in_nav_sat_fix, K_NO_WAIT);
+
+        //  publish new messages
+        uint8_t topic;
+        while (!ring_buf_is_empty(&g_msg_updates)) {
+            ring_buf_get(&g_msg_updates, &topic, 1);
+            if (topic == SYNAPSE_IN_NAVSAT_TOPIC) {
+                zbus_chan_pub(&chan_in_nav_sat_fix, &g_in_nav_sat_fix, K_NO_WAIT);
+            } else if (topic == SYNAPSE_IN_MAG_TOPIC) {
+                // zbus_chan_pub(&chan_in_mag, &g_in_mag, K_NO_WAIT);
+            } else if (topic == SYNAPSE_IN_IMU_TOPIC) {
+                zbus_chan_pub(&chan_in_imu, &g_in_imu, K_NO_WAIT);
+            }
+        }
 
         // compute board time
         uint64_t uptime = k_uptime_get();
         struct timespec ts_board;
         ts_board.tv_sec = uptime / 1.0e3;
         ts_board.tv_nsec = (uptime - ts_board.tv_sec * 1e3) * 1e6;
-        ts_board.tv_sec += g_clock_offset.seconds;
-        ts_board.tv_nsec += g_clock_offset.nanos;
+        ts_board.tv_sec += g_clock_offset.sec;
+        ts_board.tv_nsec += g_clock_offset.nanosec;
 
         // compute time delta from sim
         int64_t delta_sec = g_sim_clock.sim.sec - ts_board.tv_sec;
-        int32_t delta_nsec = g_sim_clock.sim.nsec - ts_board.tv_nsec;
+        int32_t delta_nsec = g_sim_clock.sim.nanosec - ts_board.tv_nsec;
         int64_t wait_msec = delta_sec * 1e3 + delta_nsec * 1e-6;
 
         /*

0cf944f63758d457aa9703b8bb983b473b66b62d
Peter van der Perk <peter.vanderperk@nxp.com>
2023-07-20 16:55:34 -0400

    ubx_gnss: gps2 support, 10hz rate and error handling
    Signed-off-by: Peter van der Perk <peter.vanderperk@nxp.com>
    Signed-off-by: Benjamin Perseghetti <bperseghetti@rudislabs.com>

--- drivers/sense/ubx_gnss/main.c
+++ drivers/sense/ubx_gnss/main.c
@@ -24,17 +24,33 @@ LOG_MODULE_REGISTER(ubx_gnss, CONFIG_UBX_GNSS_LOG_LEVEL);
 #define MY_STACK_SIZE 8192
 #define MY_PRIORITY 6
 
-void publish_gnss_data_zbus(uLocation_t* location)
+static int32_t gMeasurementPeriodMs = 100;
+static bool running = false;
+static bool isAlive = false;
+
+void publish_gnss_data_zbus(uDeviceHandle_t devHandle,
+    int32_t errorCode,
+    const uLocation_t* pLocation)
 {
-    synapse_msgs_NavSatFix nav_sat_fix;
+    isAlive = true;
 
-    nav_sat_fix.latitude = location->longitudeX1e7 / 1e6;
-    nav_sat_fix.longitude = location->longitudeX1e7 / 1e6;
-    nav_sat_fix.altitude = location->altitudeMillimetres / 1e3;
+    if (errorCode == 0) {
+        synapse_msgs_NavSatFix nav_sat_fix;
 
-    // TODO Covariance
+        nav_sat_fix.latitude = pLocation->latitudeX1e7 / 1e7;
+        nav_sat_fix.longitude = pLocation->longitudeX1e7 / 1e7;
+        nav_sat_fix.altitude = pLocation->altitudeMillimetres / 1e3;
 
-    zbus_chan_pub(&chan_out_nav_sat_fix, &nav_sat_fix, K_NO_WAIT);
+        // TODO Covariance
+
+        zbus_chan_pub(&chan_out_nav_sat_fix, &nav_sat_fix, K_NO_WAIT);
+        printf("lat %f long %f\n", nav_sat_fix.latitude, nav_sat_fix.longitude);
+    } else if (errorCode == U_ERROR_COMMON_TIMEOUT) {
+        LOG_ERR("Tiemout error");
+    } else {
+        LOG_ERR("GNSS error %i", errorCode);
+        running = false;
+    }
 }
 
 void sense_ubx_gnss_entry_point()
@@ -44,40 +60,58 @@ void sense_ubx_gnss_entry_point()
     // Remove the line below if you want the log printouts from ubxlib
     uPortLogOff();
     // Initiate ubxlib
-    uPortInit();
-    uDeviceInit();
     // And the U-blox GNSS module
     uDeviceHandle_t deviceHandle;
-    uDeviceGetDefaults(U_DEVICE_TYPE_GNSS, &gDeviceCfg);
 
-    gDeviceCfg.transportCfg.cfgUart.uart = 0;
-    gDeviceCfg.transportCfg.cfgUart.baudRate = 38400;
-    gDeviceCfg.deviceCfg.cfgGnss.moduleType = U_GNSS_MODULE_TYPE_M8;
+    while (true) {
+        uPortInit();
+        uDeviceInit();
 
-    errorCode = uDeviceOpen(&gDeviceCfg, &deviceHandle);
-    LOG_DBG("Opened the GNSS device %i\n", errorCode);
-    if (errorCode == 0) {
-        // Bring up the GNSS
-        errorCode = uNetworkInterfaceUp(deviceHandle, U_NETWORK_TYPE_GNSS, &gNetworkCfg);
+        uDeviceGetDefaults(U_DEVICE_TYPE_GNSS, &gDeviceCfg);
+
+        gDeviceCfg.transportCfg.cfgUart.uart = 0;
+        gDeviceCfg.transportCfg.cfgUart.baudRate = 38400;
+        gDeviceCfg.deviceCfg.cfgGnss.moduleType = U_GNSS_MODULE_TYPE_M8;
+
+        errorCode = uDeviceOpen(&gDeviceCfg, &deviceHandle);
+        LOG_DBG("Opened the GNSS device %i\n", errorCode);
         if (errorCode == 0) {
-            while (true) {
-                uLocation_t location;
-                errorCode = uLocationGet(deviceHandle, U_LOCATION_TYPE_GNSS,
-                    NULL, NULL, &location, NULL);
-                if (errorCode == 0) {
-                    publish_gnss_data_zbus(&location);
-                } else if (errorCode == U_ERROR_COMMON_TIMEOUT) {
-                    LOG_ERR("Location Timeout\n");
-                } else {
-                    LOG_ERR("Failed to get position: %d\n", errorCode);
+            // Bring up the GNSS
+            if (uNetworkInterfaceUp(deviceHandle, U_NETWORK_TYPE_GNSS, &gNetworkCfg) == 0) {
+                LOG_DBG("Starting continuous location.\n");
+                uLocationGetContinuousStart(deviceHandle,
+                    gMeasurementPeriodMs,
+                    U_LOCATION_TYPE_GNSS,
+                    NULL, NULL, publish_gnss_data_zbus);
+
+                running = true;
+
+                while (running) {
+                    uPortTaskBlock(1000);
+                    /* If cb didnt set isAlive to true hw is malfunctioning reset */
+                    if(!isAlive) {
+                        break;
+                    }
+                    /* Challange cb to set alive to ture */
+                    isAlive = false;
                 }
+
+                uLocationGetStop(deviceHandle);
+
+            LOG_DBG("Taking down GNSS...\n");
+            uNetworkInterfaceDown(deviceHandle, U_NETWORK_TYPE_GNSS);
+            uDeviceClose(deviceHandle, false);
+            } else {
+                LOG_ERR("Unable to bring up GNSS!\n");
             }
+
         } else {
-            LOG_ERR("Failed to bring up the GNSS: %d", errorCode);
+            LOG_ERR("Failed to initiate the module: %d", errorCode);
+            uPortTaskBlock(1000);
         }
-        uDeviceClose(deviceHandle, true);
-    } else {
-        LOG_ERR("Failed to initiate the module: %d", errorCode);
+
+        uDeviceDeinit();
+        uPortDeinit();
     }
 }
 

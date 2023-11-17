b0778c1374322a81f434d7fb51e8cc73dafdb5b9
James Goppert <james.goppert@gmail.com>
2023-11-17 01:32:32 -0500

    Update diffdrive to match ackermann. Use gyro in rover2d.
    Updates diffdrive to match ackermann.
    
    Also uses gyro for heading in rover2d estimator.
    
    Signed-off-by: James Goppert <james.goppert@gmail.com>

---
+++ lib/control/diffdrive/fsm.c
@@ -0,0 +1,190 @@
+/*
+ * Copyright CogniPilot Foundation 2023
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <zephyr/kernel.h>
+#include <zephyr/logging/log.h>
+
+#include <cerebri/synapse/zbus/common.h>
+#include <cerebri/synapse/zbus/syn_pub_sub.h>
+
+#define MY_STACK_SIZE 3072
+#define MY_PRIORITY 4
+
+LOG_MODULE_REGISTER(control_diffdrive, CONFIG_CEREBRI_CONTROL_DIFFDRIVE_LOG_LEVEL);
+
+typedef struct _context {
+    // node
+    syn_node_t node;
+    // data
+    synapse_msgs_Joy joy;
+    synapse_msgs_BatteryState battery_state;
+    synapse_msgs_Safety safety;
+    synapse_msgs_Fsm fsm;
+    // subscriptions
+    syn_sub_t sub_joy, sub_battery_state, sub_safety;
+    // publications
+    syn_pub_t pub_fsm;
+} context;
+
+static context g_ctx = {
+    .joy = synapse_msgs_Joy_init_default,
+    .battery_state = synapse_msgs_BatteryState_init_default,
+    .safety = synapse_msgs_Safety_init_default,
+    .fsm = {
+        .has_header = true,
+        .header = {
+            .frame_id = "base_link",
+            .has_stamp = true,
+            .seq = 0,
+            .stamp = synapse_msgs_Time_init_default,
+        },
+        .armed = synapse_msgs_Fsm_Armed_DISARMED,
+        .mode = synapse_msgs_Fsm_Mode_UNKNOWN_MODE,
+    },
+    .sub_joy = { 0 },
+    .sub_battery_state = { 0 },
+    .pub_fsm = { 0 },
+    .node = { 0 },
+};
+
+static void control_diffdrive_fsm_init(context* ctx)
+{
+    syn_node_init(&ctx->node, "control_diffdrive_fsm");
+    syn_node_add_sub(&ctx->node, &ctx->sub_joy, &ctx->joy, &chan_joy);
+    syn_node_add_sub(&ctx->node, &ctx->sub_battery_state,
+        &ctx->battery_state, &chan_battery_state);
+    syn_node_add_sub(&ctx->node, &ctx->sub_safety, &ctx->safety, &chan_safety);
+    syn_node_add_pub(&ctx->node, &ctx->pub_fsm, &ctx->fsm, &chan_fsm);
+}
+
+#define TRANSITION(STATE, GUARD, STATE_PRE, STATE_POST, LOG) \
+    if (STATE == STATE_PRE && (GUARD)) {                     \
+        STATE = STATE_POST;                                  \
+        LOG;                                                 \
+    }
+
+#define TRANSITION_FROM_ANY(STATE, GUARD, STATE_POST, LOG) \
+    if (STATE != STATE_POST && (GUARD)) {                  \
+        STATE = STATE_POST;                                \
+        LOG;                                               \
+    }
+
+static void update_fsm(
+    const synapse_msgs_Joy* joy,
+    const synapse_msgs_BatteryState* battery_state,
+    const synapse_msgs_Safety* safety,
+    synapse_msgs_Fsm* fsm)
+{
+    // fsm boolean inputs
+    bool request_arm = joy->buttons[JOY_BUTTON_ARM] == 1;
+    bool request_disarm = joy->buttons[JOY_BUTTON_DISARM] == 1;
+    bool request_manual = joy->buttons[JOY_BUTTON_MANUAL] == 1;
+    bool request_auto = joy->buttons[JOY_BUTTON_AUTO] == 1;
+    bool request_cmd_vel = joy->buttons[JOY_BUTTON_CMD_VEL] == 1;
+
+    bool mode_set = fsm->mode != synapse_msgs_Fsm_Mode_UNKNOWN_MODE;
+#ifdef CONFIG_CEREBRI_SENSE_SAFETY
+    bool safe = safety->status == synapse_msgs_Safety_Status_SAFE || safety->status == synapse_msgs_Safety_Status_UNKNOWN;
+#else
+    bool safe = false;
+#endif
+    bool battery_critical = battery_state->voltage < CONFIG_CEREBRI_CONTROL_DIFFDRIVE_BATTERY_MIN_MILLIVOLT / 1000.0;
+    bool arm_allowed = mode_set && !safe && !battery_critical;
+
+    // feedback for user
+    if (request_arm) {
+        if (!mode_set) {
+            LOG_WRN("cannot arm until mode selected");
+        }
+        if (safe) {
+            LOG_WRN("cannot arm, safety engaged");
+        }
+        if (battery_critical) {
+            LOG_WRN("cannot arm, battery critical");
+        }
+    }
+
+    // arm transitions
+    TRANSITION(
+        fsm->armed, // state
+        request_arm && arm_allowed, // guard
+        synapse_msgs_Fsm_Armed_DISARMED, // pre
+        synapse_msgs_Fsm_Armed_ARMED, // post
+        LOG_INF("armed in mode: %s", fsm_mode_str(fsm->mode));
+        LOG_INF("battery voltage: %f", battery_state->voltage););
+
+    // disarm transitions
+    TRANSITION(
+        fsm->armed, // state
+        request_disarm, // guard
+        synapse_msgs_Fsm_Armed_ARMED, // pre
+        synapse_msgs_Fsm_Armed_DISARMED, // post
+        LOG_INF("disarmed");
+        LOG_INF("battery voltage: %f", battery_state->voltage););
+
+    TRANSITION(
+        fsm->armed, // state
+        battery_critical, // guard
+        synapse_msgs_Fsm_Armed_ARMED, // pre
+        synapse_msgs_Fsm_Armed_DISARMED, // post
+        LOG_WRN("BATTERY CRITICAL: DISARMED"););
+
+    TRANSITION(
+        fsm->armed, // state
+        safe, // guard
+        synapse_msgs_Fsm_Armed_ARMED, // pre
+        synapse_msgs_Fsm_Armed_DISARMED, // post
+        LOG_WRN("SAFETY ENGAGED: DISAMRED"););
+
+    // mode transitions
+    TRANSITION_FROM_ANY(
+        fsm->mode, // state
+        request_manual, // guard
+        synapse_msgs_Fsm_Mode_MANUAL, // post
+        LOG_INF("mode manual"));
+
+    TRANSITION_FROM_ANY(
+        fsm->mode, // state
+        request_auto || request_cmd_vel, // guard
+        synapse_msgs_Fsm_Mode_CMD_VEL, // post
+        LOG_INF("mode cmd_vel/ auto"));
+
+    // set timestamp
+    stamp_header(&fsm->header, k_uptime_ticks());
+    fsm->header.seq++;
+}
+
+static void control_diffdrive_fsm_run(context* ctx)
+{
+    control_diffdrive_fsm_init(ctx);
+
+    while (true) {
+
+        // wait for joystick input event, publish at 1 Hz regardless
+        RC(syn_sub_poll(&ctx->sub_joy, K_MSEC(1000)),
+            LOG_DBG("fsm not receiving joy"));
+
+        // perform processing
+        syn_node_lock_all(&ctx->node, K_MSEC(1));
+        update_fsm(&ctx->joy, &ctx->battery_state, &ctx->safety, &ctx->fsm);
+        syn_node_publish_all(&ctx->node, K_MSEC(1));
+        syn_node_unlock_all(&ctx->node);
+    }
+}
+
+K_THREAD_DEFINE(control_diffdrive_fsm, MY_STACK_SIZE,
+    control_diffdrive_fsm_run, &g_ctx, NULL, NULL,
+    MY_PRIORITY, 0, 0);
+
+static void listener_control_diffdrive_fsm_callback(const struct zbus_channel* chan)
+{
+    syn_node_listen(&g_ctx.node, chan, K_MSEC(100));
+}
+
+ZBUS_LISTENER_DEFINE(listener_control_diffdrive_fsm, listener_control_diffdrive_fsm_callback);
+ZBUS_CHAN_ADD_OBS(chan_battery_state, listener_control_diffdrive_fsm, 1);
+ZBUS_CHAN_ADD_OBS(chan_joy, listener_control_diffdrive_fsm, 1);
+ZBUS_CHAN_ADD_OBS(chan_safety, listener_control_diffdrive_fsm, 1);
+/* vi: ts=4 sw=4 et */
--- lib/control/diffdrive/velocity.c
+++ lib/control/diffdrive/velocity.c
@@ -4,19 +4,18 @@
  */
 
 #include "casadi/rover.h"
-#include <math.h>
 
 #include <zephyr/logging/log.h>
 
 #include <cerebri/synapse/zbus/common.h>
 #include <cerebri/synapse/zbus/syn_pub_sub.h>
 
-#include "common.h"
+#include "mixing.h"
 
 #define MY_STACK_SIZE 3072
 #define MY_PRIORITY 4
 
-LOG_MODULE_DECLARE(control_ackermann);
+LOG_MODULE_DECLARE(control_diffdrive);
 
 typedef struct _context {
     syn_node_t node;
@@ -28,6 +27,8 @@ typedef struct _context {
     syn_pub_t pub_actuators;
     const double wheel_radius;
     const double wheel_base;
+    const double max_velocity;
+    const double wheel_separation;
 } context;
 
 static context g_ctx = {
@@ -40,14 +41,16 @@ static context g_ctx = {
     .sub_cmd_vel = { 0 },
     .sub_actuators_manual = { 0 },
     .pub_actuators = { 0 },
-    .wheel_radius = CONFIG_CEREBRI_CONTROL_ACKERMANN_WHEEL_RADIUS_MM / 1000.0,
-    .wheel_base = CONFIG_CEREBRI_CONTROL_ACKERMANN_WHEEL_BASE_MM / 1000.0,
+    .wheel_radius = CONFIG_CEREBRI_CONTROL_DIFFDRIVE_WHEEL_RADIUS_MM / 1000.0,
+    .wheel_base = CONFIG_CEREBRI_CONTROL_DIFFDRIVE_WHEEL_BASE_MM / 1000.0,
+    .max_velocity = CONFIG_CEREBRI_CONTROL_DIFFDRIVE_MAX_VELOCITY_MM_S / 1000.0,
+    .wheel_separation = CONFIG_CEREBRI_CONTROL_DIFFDRIVE_WHEEL_SEPARATION_MM / 1000.0,
 };
 
-static void init_control_ackermann_vel(context* ctx)
+static void init_control_diffdrive_vel(context* ctx)
 {
     LOG_DBG("init vel");
-    syn_node_init(&ctx->node, "control_ackerman_vel");
+    syn_node_init(&ctx->node, "control_diffdrive_vel");
     syn_node_add_sub(&ctx->node, &ctx->sub_cmd_vel, &ctx->cmd_vel, &chan_cmd_vel);
     syn_node_add_sub(&ctx->node, &ctx->sub_fsm, &ctx->fsm, &chan_fsm);
     syn_node_add_sub(&ctx->node, &ctx->sub_actuators_manual, &ctx->actuators_manual, &chan_actuators_manual);
@@ -57,36 +60,32 @@ static void init_control_ackermann_vel(context* ctx)
 // computes rc_input from V, omega
 void update_cmd_vel(context* ctx)
 {
-    double turn_angle = 0;
-    double omega_fwd = 0;
     double V = ctx->cmd_vel.linear.x;
     double omega = ctx->cmd_vel.angular.z;
     casadi_int* iw = NULL;
     casadi_real* w = NULL;
     int mem = 0;
-    double delta = 0;
+    double Vw = 0;
     const casadi_real* args[3];
     casadi_real* res[1];
     args[0] = &ctx->wheel_base;
     args[1] = &omega;
-    args[2] = &V;
-    res[0] = &delta;
-    ackermann_steering(args, res, iw, w, mem);
-    omega_fwd = V / ctx->wheel_radius;
-    if (fabs(V) > 0.01) {
-        turn_angle = delta;
-    }
-    ackermann_set_actuators(&ctx->actuators, turn_angle, omega_fwd);
+    args[2] = &ctx->wheel_separation;
+    res[0] = &Vw;
+    differential_steering(args, res, iw, w, mem);
+    double omega_fwd = V / ctx->wheel_radius;
+    double omega_turn = Vw / ctx->wheel_radius;
+    diffdrive_set_actuators(&ctx->actuators, omega_fwd, omega_turn);
 }
 
 static void stop(context* ctx)
 {
-    ackermann_set_actuators(&ctx->actuators, 0, 0);
+    diffdrive_set_actuators(&ctx->actuators, 0, 0);
 }
 
-static void run_control_ackermann_vel(context* ctx)
+static void run_control_diffdrive_vel(context* ctx)
 {
-    init_control_ackermann_vel(ctx);
+    init_control_diffdrive_vel(ctx);
 
     while (true) {
         int ret = 0;
@@ -131,17 +130,17 @@ static void run_control_ackermann_vel(context* ctx)
     }
 }
 
-K_THREAD_DEFINE(control_ackermann_vel, MY_STACK_SIZE,
-    run_control_ackermann_vel, &g_ctx, NULL, NULL,
+K_THREAD_DEFINE(control_diffdrive_vel, MY_STACK_SIZE,
+    run_control_diffdrive_vel, &g_ctx, NULL, NULL,
     MY_PRIORITY, 0, 0);
 
-static void listener_control_ackermann_vel_callback(const struct zbus_channel* chan)
+static void listener_control_diffdrive_vel_callback(const struct zbus_channel* chan)
 {
     syn_node_listen(&g_ctx.node, chan, K_MSEC(1));
 }
-ZBUS_LISTENER_DEFINE(listener_control_ackermann_vel, listener_control_ackermann_vel_callback);
-ZBUS_CHAN_ADD_OBS(chan_actuators_manual, listener_control_ackermann_vel, 1);
-ZBUS_CHAN_ADD_OBS(chan_fsm, listener_control_ackermann_vel, 1);
-ZBUS_CHAN_ADD_OBS(chan_cmd_vel, listener_control_ackermann_vel, 1);
+ZBUS_LISTENER_DEFINE(listener_control_diffdrive_vel, listener_control_diffdrive_vel_callback);
+ZBUS_CHAN_ADD_OBS(chan_actuators_manual, listener_control_diffdrive_vel, 1);
+ZBUS_CHAN_ADD_OBS(chan_fsm, listener_control_diffdrive_vel, 1);
+ZBUS_CHAN_ADD_OBS(chan_cmd_vel, listener_control_diffdrive_vel, 1);
 
 /* vi: ts=4 sw=4 et */

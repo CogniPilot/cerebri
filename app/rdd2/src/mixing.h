9433b59bb2284bec1039e2f9ebf6285882347feb
James Goppert <james.goppert@gmail.com>
2024-02-06 18:58:04 -0500

    Work on multirotor controller.
    Signed-off-by: James Goppert <james.goppert@gmail.com>

--- app/rdd2/src/mixing.h
+++ app/rdd2/src/mixing.h
@@ -8,7 +8,7 @@
 
 #include <synapse_topic_list.h>
 
-void rdd2_set_actuators(synapse_msgs_Actuators* msg, double turn_angle, double omega_fwd);
+void rdd2_set_actuators(synapse_msgs_Actuators* msg, double roll, double pitch, double yaw, double thrust);
 
 #endif // CEREBRI_RDD2_MIXING_H
 /* vi: ts=4 sw=4 et */

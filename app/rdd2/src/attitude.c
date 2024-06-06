1e8c60e73dfae894abccf4d5f59d9e5909d403af
Li-Yu Lin <liyu8561501@gmail.com>
2024-06-05 22:58:12 -0400

    update attitude control with loglinear
    Signed-off-by: Li-Yu Lin <liyu8561501@gmail.com>

--- app/rdd2/src/attitude.c
+++ app/rdd2/src/attitude.c
@@ -4,6 +4,7 @@
  */
 
 #include "casadi/gen/rdd2.h"
+#include "casadi/gen/rdd2_loglinear.h"
 
 #include <math.h>
 
@@ -33,8 +34,8 @@ struct context {
     synapse_msgs_Status status;
     synapse_msgs_Odometry estimator_odometry;
     synapse_msgs_Quaternion attitude_sp;
-    synapse_msgs_Vector3 angular_velocity_sp, angular_velocity_ff;
-    struct zros_sub sub_status, sub_attitude_sp, sub_estimator_odometry, sub_angular_velocity_ff;
+    synapse_msgs_Vector3 position_sp, velocity_sp, angular_velocity_sp, angular_velocity_ff;
+    struct zros_sub sub_status, sub_position_sp, sub_velocity_sp, sub_attitude_sp, sub_estimator_odometry, sub_angular_velocity_ff;
     struct zros_pub pub_angular_velocity_sp;
     atomic_t running;
     size_t stack_size;
@@ -46,10 +47,14 @@ static struct context g_ctx = {
     .node = {},
     .status = synapse_msgs_Status_init_default,
     .attitude_sp = synapse_msgs_Quaternion_init_default,
+    .position_sp = synapse_msgs_Vector3_init_default,
+    .velocity_sp = synapse_msgs_Vector3_init_default,
     .angular_velocity_sp = synapse_msgs_Vector3_init_default,
     .angular_velocity_ff = synapse_msgs_Vector3_init_default,
     .estimator_odometry = synapse_msgs_Odometry_init_default,
     .sub_status = {},
+    .sub_position_sp = {},
+    .sub_velocity_sp = {},
     .sub_attitude_sp = {},
     .sub_estimator_odometry = {},
     .pub_angular_velocity_sp = {},
@@ -64,6 +69,10 @@ static void rdd2_attitude_init(struct context* ctx)
     LOG_INF("init");
     zros_node_init(&ctx->node, "rdd2_attiude");
     zros_sub_init(&ctx->sub_status, &ctx->node, &topic_status, &ctx->status, 10);
+    zros_sub_init(&ctx->sub_position_sp, &ctx->node,
+        &topic_position_sp, &ctx->position_sp, 50);
+    zros_sub_init(&ctx->sub_velocity_sp, &ctx->node,
+        &topic_velocity_sp, &ctx->velocity_sp, 50);
     zros_sub_init(&ctx->sub_attitude_sp, &ctx->node,
         &topic_attitude_sp, &ctx->attitude_sp, 50);
     zros_sub_init(&ctx->sub_estimator_odometry, &ctx->node,
@@ -79,6 +88,8 @@ static void rdd2_attitude_fini(struct context* ctx)
     LOG_INF("fini");
     atomic_set(&ctx->running, 0);
     zros_sub_fini(&ctx->sub_status);
+    zros_sub_fini(&ctx->sub_position_sp);
+    zros_sub_fini(&ctx->sub_velocity_sp);
     zros_sub_fini(&ctx->sub_attitude_sp);
     zros_sub_fini(&ctx->sub_estimator_odometry);
     zros_sub_fini(&ctx->sub_angular_velocity_ff);
@@ -113,6 +124,14 @@ static void rdd2_attitude_run(void* p0, void* p1, void* p2)
             zros_sub_update(&ctx->sub_estimator_odometry);
         }
 
+        if (zros_sub_update_available(&ctx->sub_position_sp)) {
+            zros_sub_update(&ctx->sub_position_sp);
+        }
+
+        if (zros_sub_update_available(&ctx->sub_velocity_sp)) {
+            zros_sub_update(&ctx->sub_velocity_sp);
+        }
+
         if (zros_sub_update_available(&ctx->sub_attitude_sp)) {
             zros_sub_update(&ctx->sub_attitude_sp);
         }
@@ -122,7 +141,44 @@ static void rdd2_attitude_run(void* p0, void* p1, void* p2)
         }
 
         if (ctx->status.mode != synapse_msgs_Status_Mode_MODE_ATTITUDE_RATE) {
+            double zeta[9];
+            {
+                /* se23_error:(p_w[3],v_b[3],q_wb[4],p_rw[3],v_rw[3],q_r[4])->(zeta[9])*/
+                CASADI_FUNC_ARGS(se23_error);
+                double p_w[3], v_b[3], q_wb[4];
+                double p_rw[3], v_rw[3], q_r[4];
 
+                p_w[0] = ctx->estimator_odometry.pose.pose.position.x;
+                p_w[1] = ctx->estimator_odometry.pose.pose.position.y;
+                p_w[2] = ctx->estimator_odometry.pose.pose.position.z;
+                v_b[0] = ctx->estimator_odometry.twist.twist.linear.x;
+                v_b[1] = ctx->estimator_odometry.twist.twist.linear.y;
+                v_b[2] = ctx->estimator_odometry.twist.twist.linear.z;
+                q_wb[0] = ctx->estimator_odometry.pose.pose.orientation.w;
+                q_wb[1] = ctx->estimator_odometry.pose.pose.orientation.x;
+                q_wb[2] = ctx->estimator_odometry.pose.pose.orientation.y;
+                q_wb[3] = ctx->estimator_odometry.pose.pose.orientation.z;
+
+                p_rw[0] = ctx->position_sp.x;
+                p_rw[1] = ctx->position_sp.y;
+                p_rw[2] = ctx->position_sp.z;
+                v_rw[0] = ctx->velocity_sp.x;
+                v_rw[1] = ctx->velocity_sp.y;
+                v_rw[2] = ctx->velocity_sp.z;
+                q_r[0] = ctx->attitude_sp.w;
+                q_r[1] = ctx->attitude_sp.x;
+                q_r[2] = ctx->attitude_sp.y;
+                q_r[3] = ctx->attitude_sp.z;
+
+                args[0] = p_w;
+                args[1] = v_b;
+                args[2] = q_wb;
+                args[3] = p_rw;
+                args[4] = v_rw;
+                args[5] = q_r;
+                res[0] = zeta;
+                CASADI_FUNC_CALL(se23_error);
+            }    
             double omega[3];
             {
                 /* attitude_control:(kp[3],q[4],q_r[4])->(omega[3]) */
@@ -132,25 +188,11 @@ static void rdd2_attitude_run(void* p0, void* p1, void* p2)
                     CONFIG_CEREBRI_RDD2_YAW_KP * 1e-3,
                 };
 
-                CASADI_FUNC_ARGS(attitude_control);
-                double q[4] = {
-                    ctx->estimator_odometry.pose.pose.orientation.w,
-                    ctx->estimator_odometry.pose.pose.orientation.x,
-                    ctx->estimator_odometry.pose.pose.orientation.y,
-                    ctx->estimator_odometry.pose.pose.orientation.z,
-                };
-
-                double q_r[4] = {
-                    ctx->attitude_sp.w,
-                    ctx->attitude_sp.x,
-                    ctx->attitude_sp.y,
-                    ctx->attitude_sp.z,
-                };
+                CASADI_FUNC_ARGS(se23_attitude_control);
                 args[0] = kp;
-                args[1] = q;
-                args[2] = q_r;
+                args[1] = zeta;
                 res[0] = omega;
-                CASADI_FUNC_CALL(attitude_control);
+                CASADI_FUNC_CALL(se23_attitude_control);
             }
 
             // publish
--- app/rdd2/src/casadi/generate.sh
+++ app/rdd2/src/casadi/generate.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 poetry run -C ~/cognipilot/tools/src/cyecca python3 rdd2.py
+poetry run -C ~/cognipilot/tools/src/cyecca python3 rdd2_loglinear.py
 poetry run -C ~/cognipilot/tools/src/cyecca python3 bezier.py

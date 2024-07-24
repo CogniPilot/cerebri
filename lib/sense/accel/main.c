a7f27f286ff4b83bfdeca9e60a1fdc499c971999
James Goppert <james.goppert@gmail.com>
2024-07-23 23:51:05 -0400

    Set stream flags.
    Signed-off-by: James Goppert <james.goppert@gmail.com>

--- lib/sense/accel/main.c
+++ lib/sense/accel/main.c
@@ -6,6 +6,8 @@
 #include <zephyr/kernel.h>
 #include <zephyr/logging/log.h>
 #include <zephyr/shell/shell.h>
+#include <zephyr/rtio/rtio.h>
+#include <zephyr/drivers/sensor.h>
 
 #include <zros/private/zros_node_struct.h>
 #include <zros/private/zros_pub_struct.h>
@@ -23,6 +25,24 @@ LOG_MODULE_REGISTER(sense_accel, CONFIG_CEREBRI_SENSE_ACCEL_LOG_LEVEL);
 
 static K_THREAD_STACK_DEFINE(g_my_stack_area, MY_STACK_SIZE);
 
+/* Create a single common config for streaming */
+static struct sensor_stream_trigger iodev_accel_trigger = {
+    .opt = SENSOR_STREAM_DATA_INCLUDE,
+    .trigger = SENSOR_TRIG_FIFO_WATERMARK,
+};
+static struct sensor_read_config iodev_accel_stream_config = {
+	.sensor = NULL,
+	.is_streaming = true,
+	.triggers = &iodev_accel_trigger,
+	.count = 0,
+	.max = 1,
+};
+
+RTIO_IODEV_DEFINE(iodev_accel_stream, &__sensor_iodev_api,
+		  &iodev_accel_stream_config);
+
+RTIO_DEFINE_WITH_MEMPOOL(accel_rtio, 8, 8, 32, 64, 4);
+
 // private context
 struct context {
     struct zros_node node;
@@ -32,6 +52,7 @@ struct context {
     size_t stack_size;
     k_thread_stack_t* stack_area;
     struct k_thread thread_data;
+	struct rtio_sqe *streaming_handle;
 };
 
 // private initialization
@@ -49,7 +70,16 @@ static void sense_accel_init(struct context* ctx)
 {
     zros_node_init(&ctx->node, "sense_accel");
     zros_pub_init(&ctx->pub_accel_array, &ctx->node, &topic_accel_array_0, &ctx->accel_array);
+
+	int rc = sensor_stream(&iodev_accel_stream, &accel_rtio, &ctx,
+			       &ctx->streaming_handle);
+
+	if (rc != 0) {
+		LOG_ERR("Failed to start stream");
+	}
+
     k_sem_take(&ctx->running, K_FOREVER);
+
     LOG_INF("init");
 }
 
@@ -57,8 +87,20 @@ static void sense_accel_fini(struct context* ctx)
 {
     zros_pub_fini(&ctx->pub_accel_array);
     zros_node_fini(&ctx->node);
+
+	if (ctx->streaming_handle != NULL) {
+		LOG_ERR("Disabling stream");
+		rtio_sqe_cancel(ctx->streaming_handle);
+	}
+
     k_sem_give(&ctx->running);
     LOG_INF("fini");
+}
+
+static void accel_processing_callback(int result, uint8_t *buf, uint32_t buf_len, void *userdata)
+{
+    struct context *ctx = userdata;
+    LOG_INF("got data");
 }
 
 static void sense_accel_run(void* p0, void* p1, void* p2)
@@ -72,6 +114,10 @@ static void sense_accel_run(void* p0, void* p1, void* p2)
     while (k_sem_take(&ctx->running, K_NO_WAIT) < 0) {
 
         k_msleep(1000);
+
+		sensor_processing_with_callback(
+            &accel_rtio, accel_processing_callback);
+
         ctx->accel_array.has_stamp = true;
         stamp_msg(&ctx->accel_array.stamp, k_uptime_ticks());
         ctx->accel_array.value_count = 2;

45f750bb22e3135c071e08766cc3299aca193369
James Goppert <james.goppert@gmail.com>
2024-07-29 01:42:36 -0400

    Set stream flags.
    Signed-off-by: James Goppert <james.goppert@gmail.com>
    Signed-off-by: Benjamin Perseghetti <bperseghetti@rudislabs.com>

--- lib/sense/accel/main.c
+++ lib/sense/accel/main.c
@@ -3,8 +3,11 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+#include <zephyr/device.h>
+#include <zephyr/drivers/sensor.h>
 #include <zephyr/kernel.h>
 #include <zephyr/logging/log.h>
+#include <zephyr/rtio/rtio.h>
 #include <zephyr/shell/shell.h>
 
 #include <zros/private/zros_node_struct.h>
@@ -18,6 +21,7 @@
 
 #define MY_STACK_SIZE 4096
 #define MY_PRIORITY 4
+#define BATCH_DURATION 25
 
 LOG_MODULE_REGISTER(sense_accel, CONFIG_CEREBRI_SENSE_ACCEL_LOG_LEVEL);
 
@@ -32,6 +36,9 @@ struct context {
     size_t stack_size;
     k_thread_stack_t* stack_area;
     struct k_thread thread_data;
+    struct rtio_sqe* streaming_handle;
+    struct sensor_stream_trigger iodev_accel_trigger;
+    struct sensor_read_config iodev_accel_stream_config;
 };
 
 // private initialization
@@ -43,22 +50,177 @@ static struct context g_ctx = {
     .stack_size = MY_STACK_SIZE,
     .stack_area = g_my_stack_area,
     .thread_data = {},
+    .streaming_handle = NULL,
+    .iodev_accel_trigger = {
+        .opt = SENSOR_STREAM_DATA_INCLUDE,
+        .trigger = SENSOR_TRIG_FIFO_WATERMARK,
+    },
+    .iodev_accel_stream_config = {
+        .sensor = DEVICE_DT_GET(DT_ALIAS(accel0)),
+        .is_streaming = true,
+        .triggers = &g_ctx.iodev_accel_trigger,
+        .count = 0,
+        .max = 1,
+    },
 };
 
-static void sense_accel_init(struct context* ctx)
+static RTIO_IODEV_DEFINE(iodev_accel_stream, &__sensor_iodev_api,
+    &g_ctx.iodev_accel_stream_config);
+
+RTIO_DEFINE_WITH_MEMPOOL(accel_rtio, 4, 4, 32, 64, 4);
+
+static int sense_accel_init(struct context* ctx)
 {
     zros_node_init(&ctx->node, "sense_accel");
     zros_pub_init(&ctx->pub_accel_array, &ctx->node, &topic_accel_array_0, &ctx->accel_array);
-    k_sem_take(&ctx->running, K_FOREVER);
+
+    ctx->iodev_accel_stream_config.count = 1;
+
+    LOG_INF("device: %p", ctx->iodev_accel_stream_config.sensor);
+
+    struct sensor_value val = { BATCH_DURATION, 0 };
+    sensor_attr_set(
+        ctx->iodev_accel_stream_config.sensor,
+        SENSOR_CHAN_ALL,
+        SENSOR_ATTR_BATCH_DURATION, &val);
+
+    sensor_attr_get(
+        ctx->iodev_accel_stream_config.sensor,
+        SENSOR_CHAN_ALL,
+        SENSOR_ATTR_BATCH_DURATION, &val);
+    LOG_INF("set batch duration to: %d", val.val1);
+
+    int rc = 0;
+
+    rc = sensor_stream(&iodev_accel_stream, &accel_rtio, ctx,
+        &ctx->streaming_handle);
+
+    if (rc != 0) {
+        LOG_ERR("Failed to start stream");
+        return rc;
+    }
+
+    rc = k_sem_take(&ctx->running, K_FOREVER);
+    if (rc != 0) {
+        LOG_ERR("Failed to take running");
+        return rc;
+    }
+
     LOG_INF("init");
+    return rc;
+}
+
+double q31_to_double(int32_t q31_value, int8_t shift)
+{
+    return ((double)q31_value) / (double)(1 << (31 - shift));
 }
 
 static void sense_accel_fini(struct context* ctx)
 {
     zros_pub_fini(&ctx->pub_accel_array);
     zros_node_fini(&ctx->node);
+
+    if (ctx->streaming_handle != NULL) {
+        LOG_ERR("Disabling stream");
+        rtio_sqe_cancel(ctx->streaming_handle);
+    }
+
     k_sem_give(&ctx->running);
     LOG_INF("fini");
+}
+
+static void accel_processing_callback(int result, uint8_t* buf, uint32_t buf_len, void* userdata)
+{
+    static uint8_t decoded_buffer[127];
+    static struct {
+        uint64_t base_timestamp_ns;
+        int count;
+        uint64_t timestamp_delta;
+        int64_t values[3];
+        int8_t shift;
+    } accumulator_buffer;
+
+    struct context* ctx = userdata;
+
+    /*
+    if (ctx->iodev_accel_stream_config.sensor->api == NULL) {
+        LOG_ERR("sensor api is NULL");
+        return;
+    }
+    */
+
+    const struct sensor_decoder_api* decoder;
+    int rc = sensor_get_decoder(ctx->iodev_accel_stream_config.sensor, &decoder);
+    if (rc != 0) {
+        LOG_ERR("failed to get decoder");
+        return;
+    }
+
+    struct sensor_chan_spec ch = {
+        .chan_idx = 0,
+        .chan_type = SENSOR_CHAN_ACCEL_XYZ
+    };
+    /*
+    size_t base_size;
+    size_t frame_size;
+    uint16_t frame_count;
+    rc = decoder->get_size_info(ch, &base_size, &frame_size);
+    if (rc != 0) {
+        LOG_ERR("skipping unsupported channel accel_xyz:%d", ch.chan_idx);
+        return;
+    }
+    if (base_size > ARRAY_SIZE(decoded_buffer)) {
+        LOG_ERR("base_size > buffer size");
+        return;
+    }
+    ctx->accel_array.has_stamp = true;
+
+    if (decoder->get_frame_count(buf, ch, &frame_count) != 0) {
+        LOG_ERR("failed to get frame count");
+        return;
+    }
+    */
+
+    uint32_t fit = 0;
+    memset(&accumulator_buffer, 0, sizeof(accumulator_buffer));
+    while (decoder->decode(buf, ch, &fit, 1, decoded_buffer) > 0) {
+        struct sensor_three_axis_data* data = (struct sensor_three_axis_data*)decoded_buffer;
+        if (accumulator_buffer.count == 0) {
+            accumulator_buffer.base_timestamp_ns = data->header.base_timestamp_ns;
+            accumulator_buffer.timestamp_delta = data->readings[0].timestamp_delta;
+            accumulator_buffer.shift = data->shift;
+        }
+        accumulator_buffer.values[0] += data->readings[0].x;
+        accumulator_buffer.values[1] += data->readings[0].y;
+        accumulator_buffer.values[2] += data->readings[0].z;
+        // LOG_INF("%" PRIsensor_three_axis_data,
+        //          PRIsensor_three_axis_data_arg(*data, 0));
+        accumulator_buffer.count++;
+    }
+
+    double x = q31_to_double(accumulator_buffer.values[0] / accumulator_buffer.count, accumulator_buffer.shift);
+    double y = q31_to_double(accumulator_buffer.values[1] / accumulator_buffer.count, accumulator_buffer.shift);
+    double z = q31_to_double(accumulator_buffer.values[2] / accumulator_buffer.count, accumulator_buffer.shift);
+
+    if (accumulator_buffer.count >= 127) {
+        LOG_ERR("fifo overflow: %d", accumulator_buffer.count);
+    }
+    // LOG_INF("fifo len: %d", accumulator_buffer.count);
+
+    // LOG_INF("channel: %d, read: %d", ch.chan_idx, accumulator_buffer.count);
+    zros_pub_update(&ctx->pub_accel_array);
+    ctx->accel_array.value[0].x = x;
+    ctx->accel_array.value[0].y = y;
+    ctx->accel_array.value[0].z = z;
+    int64_t base_sec = accumulator_buffer.base_timestamp_ns / 1e9;
+    int64_t base_nano = accumulator_buffer.base_timestamp_ns - base_sec * 1e9;
+    int64_t delta_sec = accumulator_buffer.timestamp_delta / 1e9;
+    int64_t delta_nano = accumulator_buffer.timestamp_delta - delta_sec * 1e9;
+    ctx->accel_array.stamp.seconds = base_sec;
+    ctx->accel_array.stamp.nanos = base_nano;
+    ctx->accel_array.delta.seconds = delta_sec;
+    ctx->accel_array.delta.nanos = delta_nano;
+    ctx->accel_array.value_count = 1;
 }
 
 static void sense_accel_run(void* p0, void* p1, void* p2)
@@ -67,11 +229,17 @@ static void sense_accel_run(void* p0, void* p1, void* p2)
     ARG_UNUSED(p1);
     ARG_UNUSED(p2);
 
+    LOG_INF("starting");
     sense_accel_init(ctx);
 
     while (k_sem_take(&ctx->running, K_NO_WAIT) < 0) {
 
-        k_msleep(1000);
+        sensor_processing_with_callback(
+            &accel_rtio, accel_processing_callback);
+
+        /*
+        LOG_INF("publishing");
+
         ctx->accel_array.has_stamp = true;
         stamp_msg(&ctx->accel_array.stamp, k_uptime_ticks());
         ctx->accel_array.value_count = 2;
@@ -84,13 +252,17 @@ static void sense_accel_run(void* p0, void* p1, void* p2)
         ctx->accel_array.delta.nanos = 1000;
         ctx->accel_array.delta.seconds = 0;
         zros_pub_update(&ctx->pub_accel_array);
+        */
     }
+
+    LOG_INF("finished");
 
     sense_accel_fini(ctx);
 }
 
 static int start(struct context* ctx)
 {
+    LOG_INF("star called");
     k_tid_t tid = k_thread_create(&ctx->thread_data, ctx->stack_area,
         ctx->stack_size,
         sense_accel_run,
@@ -134,6 +306,7 @@ SHELL_CMD_REGISTER(sense_accel, &sub_sense_accel, "sense accel commands", NULL);
 
 static int sense_accel_sys_init(void)
 {
+    k_msleep(1000);
     return start(&g_ctx);
 };
 

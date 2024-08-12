4f62b861a1e96dab0a23813bfa5cee2fedda8d04
James Goppert <james.goppert@gmail.com>
2024-08-12 15:34:27 -0400

    Work on logging rate.
    Signed-off-by: James Goppert <james.goppert@gmail.com>

---
+++ include/cerebri/core/perf_counter.h
@@ -0,0 +1,30 @@
+#ifndef CEREBRI_CORE_PERF_COUNTER_H
+#define CEREBRI_CORE_PERF_COUNTER_H
+
+#include <zephyr/kernel.h>
+#include <stdio.h>
+
+struct perf_counter {
+    sys_snode_t node;
+    const char * name;
+    uint32_t max_period_cyc;
+    uint64_t misses;
+    uint32_t max;
+    int64_t last_cyc;
+    uint64_t delta_cyc_sum;
+    uint64_t count;
+};
+
+void perf_counter_init(struct perf_counter * counter, const char * name, double max_period_sec);
+
+void perf_counter_fini(struct perf_counter * counter);
+
+void perf_counter_update(struct perf_counter * counter);
+
+int perf_counter_report(struct perf_counter * counter, char * buf, size_t n);
+
+int perf_counter_list_report(char * buf, size_t n);
+
+// vi: ts=4 sw=4 et
+
+#endif // CEREBRI_CORE_PERF_COUNTER_H

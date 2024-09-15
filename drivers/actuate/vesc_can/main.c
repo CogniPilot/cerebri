bd4b5b879ce1035742039ba71811c89b4b0dcf90
Benjamin Perseghetti <bperseghetti@rudislabs.com>
2024-09-15 12:10:24 -0400

    Fix vesc_can.
    Signed-off-by: Benjamin Perseghetti <bperseghetti@rudislabs.com>

--- drivers/actuate/vesc_can/main.c
+++ drivers/actuate/vesc_can/main.c
@@ -28,8 +28,14 @@
 LOG_MODULE_REGISTER(actuate_vesc_can, CONFIG_CEREBRI_ACTUATE_VESC_CAN_LOG_LEVEL);
 
 #define CONFIG_VESC_CAN_ACTUATORS_INIT_PRIORITY 90
-#define MY_STACK_SIZE                      4096
-#define MY_PRIORITY                        4
+#define MY_STACK_SIZE                           4096
+#define MY_PRIORITY                             4
+
+#ifndef M_PI
+#define M_PI 3.14159265358979323846
+#endif
+
+uint32_t g_send_count = 0;
 
 extern struct perf_duration control_latency;
 
@@ -41,13 +47,10 @@ typedef enum vesc_can_type_t {
 
 typedef struct actuator_vesc_can_t {
 	const char *label;
-	bool fd;
 	uint8_t id;
 	uint8_t index;
 	uint8_t pole_pair;
-	uint8_t bus_id;
-	const struct device * device;
-	bool ready;
+	uint8_t vesc_id;
 	vesc_can_type_t type;
 } actuator_vesc_can_t;
 
@@ -61,138 +64,126 @@ struct context {
 	k_thread_stack_t *stack_area;
 	struct k_thread thread_data;
 	uint8_t num_actuators;
-	const actuator_vesc_can_t *actuator_vesc_cans;
+	actuator_vesc_can_t *actuator_vesc_cans;
+	const struct device *device;
+	bool ready;
+	bool fd;
+	const char *label;
 };
 
-static int can_actuator_stop(actuator_vesc_can_t *act)
+static int actuate_vesc_can_stop(struct context *ctx)
 {
-	int err = can_stop(act->device);
+	ctx->ready = false;
+	int err = can_stop(ctx->device);
 	if (err != 0) {
-		LOG_ERR("can%d - failed to stop (%d)\n", act->bus_id, err);
-		act->ready = false;
+		LOG_ERR("%s - failed to stop (%d)\n", ctx->label, err);
 		return err;
 	}
 	return err;
 }
 
-static int can_actuator_start(actuator_vesc_can_t *act)
+static int actuate_vesc_can_init(struct context *ctx)
 {
+	int err = 0;
+	zros_node_init(&ctx->node, "actuate_vesc_can");
+	zros_sub_init(&ctx->sub_actuators, &ctx->node, &topic_actuators, &ctx->actuators, 1000);
+	zros_sub_init(&ctx->sub_status, &ctx->node, &topic_status, &ctx->status, 10);
+
 	enum can_state state;
 	struct can_bus_err_cnt err_cnt;
-	int err = 0;
 
-	if (act->ready) {
+	if (ctx->ready) {
+		LOG_ERR("%s - already initialized", ctx->label);
 		return 0;
 	}
 
 	// check if device ready
-	if (!device_is_ready(act->device)) {
-		LOG_ERR("can%d - device not ready\n", act->bus_id);
-		act->ready = false;
+	if (!device_is_ready(ctx->device)) {
+		LOG_ERR("%s - device not ready\n", ctx->label);
+		ctx->ready = false;
 		return -1;
 	} // check state
-	err = can_get_state(act->device, &state, &err_cnt);
+	err = can_get_state(ctx->device, &state, &err_cnt);
 	if (err != 0) {
-		LOG_ERR("can%d - failed to get CAN controller state (%d)\n",
-			act->bus_id, err);
-		act->ready = false;
+		LOG_ERR("%s - failed to get CAN controller state (%d)\n", ctx->label, err);
+		ctx->ready = false;
 		return err;
 	}
 
 	// set device mode
-	if (act->fd) {
+	if (ctx->fd) {
 		if (state != CAN_STATE_STOPPED) {
-			can_actuator_stop(act);
+			actuate_vesc_can_stop(ctx);
 		}
-		err = can_set_mode(act->device, CAN_MODE_FD);
+		err = can_set_mode(ctx->device, CAN_MODE_FD);
 		if (err != 0) {
-			LOG_ERR("can%d - set mode FD failed (%d)\n", act->bus_id, err);
-			can_actuator_stop(act);
+			LOG_ERR("%s - set mode FD failed (%d)\n", ctx->label, err);
 			return err;
 		}
 	}
 
 	// start device
 	if ((state == CAN_STATE_STOPPED) || (state == CAN_STATE_BUS_OFF)) {
-
-		err = can_start(act->device);
+		err = can_start(ctx->device);
 		if (err != 0) {
-			LOG_ERR("can%d - start failed\n", act->bus_id);
-			can_actuator_stop(act);
+			LOG_ERR("%s - start failed\n", ctx->label);
 			return err;
 		}
 	}
 
-	act->ready = true;
-	LOG_DBG("can%d - connected and properly initialized.\n", act->bus_id);
-	return 0;
-}
-
-static int actuate_vesc_can_init(struct context *ctx)
-{
-	LOG_INF("init");
-	int err = 0;
-
-	for (int i = 0; i < ctx->num_actuators; i++) {
-		actuator_vesc_can_t act = ctx->actuator_vesc_cans[i];
-		k_usleep(1e6 / 1);
-		err =  can_actuator_start(&act);
-		if (err != 0) {
-			return err;
-		}
-	}
-
-	zros_node_init(&ctx->node, "actuate_vesc_can");
-	zros_sub_init(&ctx->sub_actuators, &ctx->node, &topic_actuators, &ctx->actuators, 1000);
-	zros_sub_init(&ctx->sub_status, &ctx->node, &topic_status, &ctx->status, 10);
+	ctx->ready = true;
+	LOG_DBG("%s - connected and properly initialized.\n", ctx->label);
 	k_sem_take(&ctx->running, K_FOREVER);
+	LOG_INF("init");
 	return 0;
 }
 
 static int actuate_vesc_can_fini(struct context *ctx)
 {
-	LOG_INF("fini");
-
-	for (int i = 0; i < ctx->num_actuators; i++) {
-		actuator_vesc_can_t act = ctx->actuator_vesc_cans[i];
-		can_actuator_stop(&act);
-	}
+	actuate_vesc_can_stop(ctx);
 	zros_sub_fini(&ctx->sub_actuators);
 	zros_sub_fini(&ctx->sub_status);
 	zros_node_fini(&ctx->node);
 	k_sem_give(&ctx->running);
+	LOG_INF("fini");
 	return 0;
 }
 
-static void vesc_can_update(struct context *ctx)
+static void actuate_vesc_can_update(struct context *ctx)
 {
 	bool armed = ctx->status.arming == synapse_pb_Status_Arming_ARMING_ARMED;
 	int err = 0;
 
 	for (int i = 0; i < ctx->num_actuators; i++) {
-		actuator_vesc_can_t vesc_can = ctx->actuator_vesc_cans[i];
+		actuator_vesc_can_t *vesc_can = &ctx->actuator_vesc_cans[i];
 
-		uint32_t pulse = 0; //= vesc_can.disarmed;
+		struct can_frame frame = {
+			.dlc = can_bytes_to_dlc(4),
+			.flags = CAN_FRAME_IDE,
+		};
 		double input = 0;
 
-		if (vesc_can.type == VESC_CAN_TYPE_NORMALIZED) {
-			input = ctx->actuators.normalized[vesc_can.index];
-		} else if (vesc_can.type == VESC_CAN_TYPE_POSITION) {
-			input = ctx->actuators.position[vesc_can.index];
-		} else if (vesc_can.type == VESC_CAN_TYPE_VELOCITY) {
-			input = ctx->actuators.velocity[vesc_can.index];
+		if (vesc_can->type == VESC_CAN_TYPE_VELOCITY && armed) {
+			input = ctx->actuators.velocity[vesc_can->index];
 		}
 
-		if (armed) {
-			//pulse = (uint32_t)((vesc_can.scale * input) + vesc_can.center);
-		}
+		int32_t erpm = vesc_can->pole_pair * input * 60 / (2 * M_PI);
+		frame.id = 768 + vesc_can->vesc_id;
+		frame.data[0] = erpm >> 24 & 255;
+		frame.data[1] = erpm >> 16 & 255;
+		frame.data[2] = erpm >> 8 & 255;
+		frame.data[3] = erpm & 255;
 
-		//err = vesc_can_set_pulse_dt(&vesc_can.device, VESC_CAN_NSEC(pulse));
+		// send can data
+		g_send_count += 1;
+		err = can_send(ctx->device, &frame, K_NO_WAIT, NULL, NULL);
+		if (err != 0) {
+			ctx->ready = false;
+			LOG_ERR("%s - send failed to VESC ID: %d (%d)\n", vesc_can->label,
+				vesc_can->vesc_id, err);
+			continue;
+		}
 		perf_duration_stop(&control_latency);
-
-		if (err) {
-			LOG_ERR("Failed to set pulse %d on %d (err %d)", pulse, vesc_can.index, err);
-		}
 	}
 }
 
@@ -205,6 +196,7 @@ static void actuate_vesc_can_run(void *p0, void *p1, void *p2)
 	int ret = actuate_vesc_can_init(ctx);
 
 	if (ret < 0) {
+		actuate_vesc_can_fini(ctx);
 		return;
 	}
 
@@ -220,7 +212,7 @@ static void actuate_vesc_can_run(void *p0, void *p1, void *p2)
 			// put motors in disarmed state
 			if (ctx->status.arming == synapse_pb_Status_Arming_ARMING_ARMED) {
 				ctx->status.arming = synapse_pb_Status_Arming_ARMING_DISARMED;
-				LOG_ERR("disarming motors due to actuator msg timeout!");
+				LOG_ERR("Disarming motors due to actuator msg timeout!");
 			}
 		}
 
@@ -233,7 +225,7 @@ static void actuate_vesc_can_run(void *p0, void *p1, void *p2)
 		}
 
 		// update vesc_can
-		vesc_can_update(ctx);
+		actuate_vesc_can_update(ctx);
 	}
 
 	actuate_vesc_can_fini(ctx);
@@ -242,14 +234,16 @@ static void actuate_vesc_can_run(void *p0, void *p1, void *p2)
 static int start(struct context *ctx)
 {
 	__ASSERT_NO_MSG(ctx != NULL);
-	k_tid_t tid = k_thread_create(&ctx->thread_data, ctx->stack_area, ctx->stack_size,
-				      actuate_vesc_can_run, ctx, NULL, NULL, MY_PRIORITY, 0, K_FOREVER);
+	k_tid_t tid =
+		k_thread_create(&ctx->thread_data, ctx->stack_area, ctx->stack_size,
+				actuate_vesc_can_run, ctx, NULL, NULL, MY_PRIORITY, 0, K_FOREVER);
 	k_thread_name_set(tid, "actuate_vesc_can");
 	k_thread_start(tid);
 	return 0;
 }
 
-static int actuate_vesc_can_cmd_handler(const struct shell *sh, size_t argc, char **argv, void *data)
+static int actuate_vesc_can_cmd_handler(const struct shell *sh, size_t argc, char **argv,
+					void *data)
 {
 	ARG_UNUSED(argc);
 	struct context *ctx = data;
@@ -286,25 +280,21 @@ static int actuate_vesc_can_device_init(const struct device *dev)
 	SHELL_SUBCMD_DICT_SET_CREATE(sub_actuate_vesc_can_##inst, actuate_vesc_can_cmd_handler,    \
 				     (start, &data_##inst, "start"), (stop, &data_##inst, "stop"), \
 				     (status, &data_##inst, "status"));                            \
-	SHELL_CMD_REGISTER(actuate_vesc_can_##inst, &sub_actuate_vesc_can_##inst, "actuate_vesc_can commands", \
-			   NULL);
+	SHELL_CMD_REGISTER(actuate_vesc_can_##inst, &sub_actuate_vesc_can_##inst,                  \
+			   "actuate_vesc_can commands", NULL);
 
 #define VESC_CAN_ACTUATOR_DEFINE(node_id)                                                          \
 	{                                                                                          \
 		.label = DT_NODE_FULL_NAME(node_id),                                               \
-		.fd = DT_PROP(node_id, fd),                                                        \
 		.index = DT_PROP(node_id, input_index),                                            \
 		.pole_pair = DT_PROP(node_id, pole_pair),                                          \
-		.bus_id = DT_PROP(node_id, bus_id),                                                \
-		.device = DEVICE_DT_GET(DT_PROP(node_id, device)),                                 \
+		.vesc_id = DT_PROP(node_id, vesc_id),                                              \
 		.type = DT_ENUM_IDX(node_id, input_type),                                          \
-		.ready = true,                                                                     \
 	},
 
 #define VESC_CAN_ACTUATORS_DEFINE(inst)                                                            \
-	static const actuator_vesc_can_t g_actuator_vesc_cans_##inst[] = {                         \
-		DT_FOREACH_CHILD(                                                                  \
-			DT_INST(inst, cerebri_vesc_can_actuators), VESC_CAN_ACTUATOR_DEFINE)};     \
+	static actuator_vesc_can_t g_actuator_vesc_cans_##inst[] = {                               \
+		DT_FOREACH_CHILD(DT_DRV_INST(inst), VESC_CAN_ACTUATOR_DEFINE)};                    \
 	static K_THREAD_STACK_DEFINE(g_my_stack_area_##inst, MY_STACK_SIZE);                       \
 	static struct context data_##inst = {                                                      \
 		.actuators = synapse_pb_Actuators_init_default,                                    \
@@ -317,7 +307,11 @@ static int actuate_vesc_can_device_init(const struct device *dev)
 		.stack_area = g_my_stack_area_##inst,                                              \
 		.thread_data = {},                                                                 \
 		.actuator_vesc_cans = g_actuator_vesc_cans_##inst,                                 \
-		.num_actuators = DT_CHILD_NUM(DT_INST(inst, cerebri_vesc_can_actuators)),          \
+		.num_actuators = DT_CHILD_NUM(DT_DRV_INST(inst)),                                  \
+		.device = DEVICE_DT_GET(DT_INST_PROP(inst, device)),                               \
+		.ready = false,                                                                    \
+		.fd = DT_INST_PROP(inst, fd),                                                      \
+		.label = DT_NODE_FULL_NAME(DT_DRV_INST(inst)),                                     \
 	};                                                                                         \
 	VESC_CAN_ACTUATOR_SHELL(inst);                                                             \
 	DEVICE_DT_INST_DEFINE(inst, actuate_vesc_can_device_init, NULL, &data_##inst, NULL,        \

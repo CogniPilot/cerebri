6f20f1aaf2cdd3a0df0c8365d6309ae1296f731d
Luis Ubieda <luisf@croxel.com>
2025-10-31 14:32:36 -0400

    zros: offload perf utilities to zros-drivers repo
    Updated manifest to go in sync with changes.
    
    Signed-off-by: Luis Ubieda <luisf@croxel.com>

--- include/cerebri/core/perf_counter.h
+++ include/cerebri/core/perf_counter.h
@@ -1,30 +1,9 @@
 #ifndef CEREBRI_CORE_PERF_COUNTER_H
 #define CEREBRI_CORE_PERF_COUNTER_H
 
-#include <zephyr/kernel.h>
-
-struct perf_counter {
-	sys_snode_t node;
-	const char *name;
-	uint32_t deadline_cyc;
-	uint32_t min_period_cyc;
-	uint32_t max_period_cyc;
-	uint64_t misses;
-	int64_t last_cyc;
-	uint64_t delta_cyc_sum;
-	uint64_t count;
-};
-
-void perf_counter_init(struct perf_counter *counter, const char *name, double max_period_sec);
-
-void perf_counter_fini(struct perf_counter *counter);
-
-void perf_counter_update(struct perf_counter *counter);
-
-int perf_counter_report(struct perf_counter *counter, char *buf, size_t n);
-
-void perf_counter_list_report(char *buf, size_t n);
-
-// vi: ts=4 sw=4 et
+/** This file has been moved to the common ZROS drivers. The header is kept
+ * for backwards compability.
+ */
+#include <zros/perf_counter.h>
 
 #endif // CEREBRI_CORE_PERF_COUNTER_H
